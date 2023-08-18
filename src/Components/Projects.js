085091b55ca4b11ebe27a85389f87a22ef1a7ce6
Gillesedgy <edgygilles@pursuit.org>
2023-08-18 18:16:17 -0400

    Created a data file for projects.. only update I'll make today

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,43 +1,12 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { projectData } from "../assets/data/projectsData";
 import "../Styles/Projects.scss";
 function Projects() {
-  const navigate = useNavigate();
-  const myProjects = [
-    {
-      title: "dotCoin",
-      image: "🏞️",
-      description:
-        "dotCoin is a user-friendly app designed to help users track their daily expenses and income. It features customizable budget categories, real-time expense tracking, and insightful reports to visualize spending habits. Users can set budget goals and receive alerts when they're close to their limits",
-      url: "https://google.com/",
-    },
-    {
-      title: "Not-It",
-      image: "🏞️",
-      description:
-        "Not-It is a sleek and intuitive note-taking app that allows users to organize their thoughts and ideas efficiently. Features include rich text editing, note categorization, and a powerful search function.",
-      url: "https://google.com/",
-    },
-    {
-      title: "ShopStreet",
-      image: "🏞️",
-      description:
-        "ShopStreet is an e-commerce platform designed for small businesses...",
-      url: "https://google.com/",
-    },
-    {
-      title: "GhibliGrub",
-      image: "🏞️",
-      description:
-        "GhibliGrub is a unique app that brings the magical animation of Studio Ghibli films to life...",
-      url: "https://google.com/",
-    },
-  ];
+  const myProjects = projectData();
 
   // Todo: Import Images and update URLs / Create Cards for each project => project should display and be used within app (pop up).
   return (
     <div className="Projects">
-
       {myProjects.map((project) => (
         <div key={project.title} className="Projects__card">
           <a href={project.url}>
---
+++ src/assets/data/projectsData.js
@@ -0,0 +1,32 @@
+export function projectData() {
+  return [
+    {
+      title: "dotCoin",
+      image: "🏞️",
+      description:
+        "dotCoin is a user-friendly app designed to help users track their daily expenses and income. It features customizable budget categories, real-time expense tracking, and insightful reports to visualize spending habits. Users can set budget goals and receive alerts when they're close to their limits",
+      url: "https://google.com/",
+    },
+    {
+      title: "Not-It",
+      image: "🏞️",
+      description:
+        "Not-It is a sleek and intuitive note-taking app that allows users to organize their thoughts and ideas efficiently. Features include rich text editing, note categorization, and a powerful search function.",
+      url: "https://google.com/",
+    },
+    {
+      title: "ShopStreet",
+      image: "🏞️",
+      description:
+        "ShopStreet is an e-commerce platform designed for small businesses...",
+      url: "https://google.com/",
+    },
+    {
+      title: "GhibliGrub",
+      image: "🏞️",
+      description:
+        "GhibliGrub is a unique app that brings the magical animation of Studio Ghibli films to life...",
+      url: "https://google.com/",
+    },
+  ];
+}
