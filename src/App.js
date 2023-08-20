58827cce9fc5e468c4b5bcdb328626c6f51a4dba
Gillesedgy <edgygilles@pursuit.org>
2023-08-19 20:54:00 -0400

    Decided to Create SPA instead of MPA. Updated components and created the side effect for the scroll animation.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  // Navbar,
+  Home,
+  About,
+  Skills,
+  Projects,
+  Contact,
+} from "./Components/Container/components";
 import NavBar from "./Components/NavBar";
-import Home from "./Components/Home";
-import About from "./Components/About";
-import Skills from "./Components/Skills";
-import Projects from "./Components/Projects";
-import Contact from "./Components/Contact";
 function App() {
   //Todo: hide navbar or  something.. not sure yet.. looks good for now
   return (
     <div className="App">
       <Router>
         <NavBar />
-        <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/skills" element={<Skills />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+        <section id="home">
+          <Home />
+        </section>
+        <section id="about">
+          <About />
+        </section>
+        <section id="skills">
+          <Skills />
+        </section>
+        <section id="projects">
+          <Projects />
+        </section>
+        <section id="contact">
+          <Contact />
+        </section>
       </Router>
     </div>
   );
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import "../Styles/NavBar.scss";
 /**
@@ -10,50 +10,68 @@ import "../Styles/NavBar.scss";
  */
 function NavBar() {
   const routerLocation = useLocation();
-  //  is a constant that stores the location object returned by useLocation()
+  //  is a constant that stores the location object returned by useLocation() => contains URL information
   const setActive = (pathname) => {
     return routerLocation.pathname === pathname;
-  };
-  //* set Active checks if the pathName given as the arg is equal to the current pathName in the router location. if true=> updates assight the nav active class. else=> nada
+  }; //* set Active checks if the pathName given as the arg is equal to the current pathName in the router location. if true=> updates assight the nav active class. else=> nada..
 
+  useEffect(() => {
+    const hash = routerLocation.hash;
+    if (hash) {
+      const section = document.querySelector(hash);
+      if (section) {
+        section.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  }, [routerLocation]);
+  /**
+   * TLDR => https://reactrouter.com/en/main/hooks/use-location
+   * hash => part of the url that contains the #
+   * UseEffect used to handle the smooth scroll side effect and only updates when the location or path name changes.
+   *   - it checks if theres a hash value in the URL before running the code. if not, it returns null
+   *   - if theres is an hash value assigned, it grabs it and matches it with the corresponding 'id' respectively (see components ins App.JS).
+   *   -
+   */
+  //! TODO: Must add a function that updates the indicator on scroll as well 
   return (
+    // The "#" symbols is what allows the URL to properly work.. IT is what tell sit which 'id' to target.  like the name says hash to hash lol.
     <nav className="Navbar">
       <Link
-        to="/"
+        to="/#home"
         className={`Navbar__link ${
-          setActive("/") ? "Navbar__link--active" : ""
+          setActive("/#home") ? "Navbar__link--active" : ""
         }`}
       >
         <h1 className="Navbar__title">Edgy Gilles</h1>
       </Link>
       <Link
-        to="/about"
+        to="/#about"
         className={`Navbar__link ${
-          setActive("/about") ? "Navbar__link--active" : ""
+          setActive("/#about") ? "Navbar__link--active" : ""
         }`}
       >
         About
       </Link>
       <Link
-        to="/skills"
+        to="/#skills"
         className={`Navbar__link ${
-          setActive("/skills") ? "Navbar__link--active" : ""
+          setActive("/#skills") ? "Navbar__link--active" : ""
         }`}
       >
         Skills
       </Link>
       <Link
-        to="/projects"
+        to="/#projects"
         className={`Navbar__link ${
-          setActive("/projects") ? "Navbar__link--active" : ""
+          setActive("/#projects") ? "Navbar__link--active" : ""
         }`}
       >
         Projects
       </Link>
       <Link
-        to="/contact"
+        to="/#contact"
         className={`Navbar__link ${
-          setActive("/contact") ? "Navbar__link--active" : ""
+          setActive("/#contact") ? "Navbar__link--active" : ""
         }`}
       >
         Contact
